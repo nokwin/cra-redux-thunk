d1c846f4c34fcc7fd9c62e28ddbe6f4e51ae47fd
Dmytro Batarin <dmytro.jr@batarin.zp.ua>
2020-06-18 23:15:02 +0300

    Added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import { HomePage } from "./pages/homepage/homepage.component";
 
 function App() {
-  return <div>nothing</div>;
+  return (
+    <BrowserRouter>
+      <Route exact path="/" component={HomePage} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
