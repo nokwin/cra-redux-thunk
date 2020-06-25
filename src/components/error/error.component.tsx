40744c0db6c622094a63f84f1a5f93fa1cf8ba43
Dmytro Batarin <dmytro.jr@batarin.zp.ua>
2020-06-25 20:11:52 +0300

    Moved everything to template folder

--- src/components/error/error.component.tsx
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-export const Error = () => <h1>Oooops! Something went wrong</h1>;
--- src/redux/counter/counter.actions.ts
+++
@@ -1,9 +0,0 @@
-import { INCREMENT, DECREMENT, CounterActionTypes } from "./counter.types";
-
-export const incrementAction = (): CounterActionTypes => ({
-  type: INCREMENT,
-});
-
-export const decrementAction = (): CounterActionTypes => ({
-  type: DECREMENT,
-});
