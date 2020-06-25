4ff467dc3161287a55714d67727acb05f9e2c909
Dmytro Batarin <dmytro.jr@batarin.zp.ua>
2020-06-25 21:50:55 +0300

    Header test

--- template/src/components/header/header.test.ts
+++
@@ -1,7 +0,0 @@
-import { Header } from "./header.component";
-
-describe("Header component", () => {
-  it("should render header component", () => {
-    expect(Header).toMatchSnapshot();
-  });
-});
